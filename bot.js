de15019aac64b7a694a05f9466e295a69ac6d8ec
Jikol <gikol113@gmail.com>
2017-10-22 15:31:21 +0200

    test

--- bot.js
+++ bot.js
@@ -11,4 +11,10 @@ client.on('message', message => {
   }
 });
 
+client.on('message', message => {
+  if(message.content === 'ahoj') {
+    message.reply('ahoj');
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
