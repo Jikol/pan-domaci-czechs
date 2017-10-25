5c6df3f3d46e0e2c54b7de25e706bc465b0fa95c
Jikol <32988071+Jikol@users.noreply.github.com>
2017-10-25 18:36:30 +0200

    .

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const discord = require('discord.js');
 const bot = new discord.Client();
+const TOKEN = "MzQ2NDE4NjMzNjg3MzAyMTQ2.DNJOfw.kTjJ0S5ayhzL5wRy8GBKQEpfmjI";
 
 bot.on('message', (message) => {
     if (message.content == '.test') {
@@ -7,6 +8,5 @@ bot.on('message', (message) => {
     }
 });
 
-
+bot.login(TOKEN);
 bot.login(process.env.BOT_TOKEN);
-bot.login("MzQ2NDE4NjMzNjg3MzAyMTQ2.DNJNSg.WOLWsxKMpfgBTUAiU5twI5xr6Lc");
