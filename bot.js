cecab22b8f11b2a91353ce1a387ef67a8c25b8b8
Jikol <32988071+Jikol@users.noreply.github.com>
2017-10-25 07:52:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,11 +8,12 @@ client.on('ready', () => {
 client.on('message', function(message) {
   if(message.content == ".clear") {
     if(message.member.hasPermission("MANAGE_MESSAGES")) {
-      message.channel.fetchMessages().then(function(list) {
-        message.channel.bulkDelete(list);  
-      }, function(err) {
+      message.channel.fetchMessages()
+        .then(function(list) {
+          message.channel.bulkDelete(list);  
+        }, function(err) {
           message.channel.send("ERROR: ERROR CLEARING CHANNEL.")
-      })                                           
+        })                                           
     }
   }
 });
