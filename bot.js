9bd0b7123d5ced1225ea02dc9b78c203b74baeca
Jikol <32988071+Jikol@users.noreply.github.com>
2017-11-11 16:20:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var bot = new discord.Client();
 
 bot.on('ready', () => {
   console.log(message.content);  
-  bot.user.setGame("use !help");
+  bot.user.setGame("pouzij .help");
   console.log("Pripraven");  
 });
 
