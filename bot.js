d402bf2c0273ecf2730d7e5862b902c6bbd532f2
Jikol <32988071+Jikol@users.noreply.github.com>
2018-07-12 19:58:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,17 @@ bot.on('message', (message) => {
                 {
                     function vypis() {
                         message.delete();
-                        message.channel.send("Pro více info se ptej rank Zástupce a víš...");
+                        const embed = new discord.RichEmbed()
+                            .setTitle("Facebook")
+                            .setAuthor("Czechs [CZ]", "https://i.imgur.com/MxduEjo.png")
+                            .setColor(0x00ba25)
+                            .setFooter("Jikol.7265 |", "https://i.imgur.com/3nZV0be.png")
+                            .setThumbnail("https://i.imgur.com/JxHPRoN.jpg")
+                            .setTimestamp()
+                            .setURL("https://www.facebook.com/groups/CzechsCZ/")
+                            .addField("Vítej na našem Discordu!", "Zde cilíme naši hlavní komunikaci, jak už s domlouváním raidů a fraktálů, tak s radami a polemizováním nad aktuální situací ve světě Guild Wars! \n\nPro více info se ptej rank Zástupce a víš... \n\n**Chovej se zde slušně!**")
+                            .setImage("https://i.imgur.com/RHXpi3R.png")
+                        message.channel.sendEmbed(embed);
                     }
                     vypis();
                     break;
@@ -27,10 +37,11 @@ bot.on('message', (message) => {
                         message.delete();
 
                         if (!message.member.roles.find("name", "Zástupce")) {
-                            var embed = new Discord.RichEmbed()
+                            var embed = new discord.RichEmbed()
                                 .addField("Něco se pokazilo...", "Jenom zástupci mohou mazat zprávy!")
                                 .setColor(0x00ba25)
                                 .setFooter("Pro více info napiš .pomoc")
+                                .setThumbnail("https://cdn.pixabay.com/photo/2013/07/12/13/21/delete-146891_960_720.png")
                             message.channel.sendEmbed(embed);
                             return;
                         }
