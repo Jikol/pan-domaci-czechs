30a15028bac187440d94fffa1bcd8b74472dc1aa
Jikol <32988071+Jikol@users.noreply.github.com>
2018-07-13 19:43:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,11 +50,10 @@ bot.on('message', (message) => {
                             return;
                         }
 
-                        let pocet = zprava[1] + 1;
                         const fetched = await message.channel.fetchMessages({
-                            limit: pocet
+                            limit: zprava[1]
                         });
-                        message.channel.send(fetched);
+                        message.channel.send(fetched.size);
                         console.log(fetched.size + ' zprav bude smazano');
                         
                         message.channel.bulkDelete(fetched).catch(error => message.channel.send(`Error: ${error}`));
